85f724fbd8bce893f05cc8c226eb911d6f8451c4
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2025-01-22 20:51:26 +0100

    js web only funcionality
    nothing about main content only the forms

---
+++ JsWeb/js/comment.js
@@ -0,0 +1,58 @@
+const button = document.getElementsByTagName("button"); // cogo el elemento del boton
+const url = "http://localhost:8080/apiWeb/addComment" // pongo la url de login del back 
+
+//tras coger el primer boton del html (el unico que hay) escuchamos el evento click y ejecutamso la funcion
+button[0].addEventListener("click", (event) => {
+    addComment(event);
+});
+
+function addComment(event){
+    event.preventDefault();// prevenimos el efecto de recargar del boton
+
+    const request = new Map(); //generamos un map para despues hacerlo JSON para la llamada
+    request.set("idUser",sessionStorage.getItem("iduser")); // Añadimos el id del usuario que guardamos anteriormente en la session
+
+    let message = document.getElementById("message").value; // cogemos el valor del mensaje
+    request.set("comment",message); // añadimos el valor del mensaje 
+
+    let inputs = document.getElementsByTagName("input");//cogemos todos los inputs de la pagina 
+    for(let i=0;i<inputs.length;i++){// recoremos los inputs ya que hay mas de un input
+        request.set(inputs[i].id,inputs[i].value);// por cada input añadimos el valor en el map
+    }
+    
+    callApi(request);
+
+}
+
+async function callApi(request){
+    
+    const obj = Object.fromEntries(request); // lo convertimos en String (una forma extraña de convertir en String)
+    const json = JSON.stringify(obj); // pasamos ese String a un formato json
+ 
+    let response = await fetch(url,{
+        method : 'post',// el metodo de la llamada
+        body: json, // el body que es el json que hemos generado ya que es todos los datos del comentario
+        headers: {
+            "Content-Type": "application/json", // esto es el tipo de header para que sepa que lo que hay en el body es un json
+          }
+
+    }); // hacemos la llamada a la url
+
+    if(response.ok){// comprobamos que la llamada ha salido bien
+        callsuccess();
+    }else{
+        callFail("Internal Error");
+    }
+}
+
+function callerror(message){
+    let error = document.getElementById("error");// cogemos la p del error
+    error.hidden = false;// lo mostramos en la pagina
+    error.innerHTML = message;// metemos el texto dentro de la p
+}
+
+function callsuccess(){
+    let success = document.getElementById("success"); // cogemos la p del success
+    success.hidden= false;// lo mostramos en la pagina
+    success.innerHTML = "The comment has been added successfully";// metemos el texto dentro de la p
+}
--- SpringBoot/api/src/main/java/com/web/api/controller/WebController.java
+++ SpringBoot/api/src/main/java/com/web/api/controller/WebController.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j //para poner logs
-@CrossOrigin //para que se pueda llamar desde cualquier sitio
+@CrossOrigin("*") //para que se pueda llamar desde cualquier sitio
 @RestController //para hacer llamadas a la api y hacer una respuesta por el body
 @RequiredArgsConstructor //para que se inyecten los beans
 @RequestMapping("/apiWeb") //mapeo de la url que aparece al hacer la llamada
@@ -63,6 +63,7 @@ public class WebController {
      */
     @PostMapping("/addComment") 
     public ResponseEntity<String> addComment(@RequestBody CommentCallDto commentCallDto) {
+        log.info(commentCallDto.toString());
         return webService.addComment(commentCallDto);
     }
 
